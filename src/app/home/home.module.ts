69b7e1c18cf132394e11db49117077fb75fa639c
madhan731 <madhankumarms0702@gmail.com>
2022-07-24 15:38:18 +0530

    4 files modified, 3 added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { Page2Component } from './page2/page2.component';
 import { HomeComponent } from './home.component';
 import { ParentModule } from '../parent/parent.module';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @NgModule({
   imports: [HomeRoutingModule,ParentModule],
-  declarations: [HomeComponent,Page1Component,Page2Component,HeaderComponent],
+  declarations: [HomeComponent,Page1Component,Page2Component,HeaderComponent,FooterComponent],
 })
 export class HomeModule {}

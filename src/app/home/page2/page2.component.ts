dba3c1c8d1d1c7c5a9ed6a47df286d4fc4e64ddf
madhan731 <madhankumarms0702@gmail.com>
2022-07-24 15:22:58 +0530

    3 files modified

--- src/app/home/page2/page2.component.ts
+++ src/app/home/page2/page2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page2Component implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+ showpp()
+ {
+    this.router.navigate(['/page1'])
+ }
 }

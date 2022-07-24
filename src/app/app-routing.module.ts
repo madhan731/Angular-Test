64be30ad6e1bbdf580b64c96d1dce99279e011c6
madhan731 <madhankumarms0702@gmail.com>
2022-07-24 14:49:00 +0530

    4 files modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,15 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((x) => x.HomeModule),
   },
   {
     path: 'process',
-    loadChildren: () => import('./process/process.module').then((x) => x.ProcessModule),
+    loadChildren: () =>
+      import('./process/process.module').then((x) => x.ProcessModule),
   },
- 
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { HomeComponent } from './home.component';
 import { ParentModule } from '../parent/parent.module';
+import { HeaderComponent } from '../header/header.component';
 
 @NgModule({
   imports: [HomeRoutingModule,ParentModule],
-  declarations: [HomeComponent,Page1Component,Page2Component],
+  declarations: [HomeComponent,Page1Component,Page2Component,HeaderComponent],
 })
 export class HomeModule {}
